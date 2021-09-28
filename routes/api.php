2d8491309c1896bbbfef469a2c55f72da1e1cabe
lucypeng <lucypeng60@gmail.com>
2021-09-28 22:57:10 +0700

    challange author

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,4 @@ use App\Http\Controllers\BookController;
 
 Route::get('/me', [AuthController::class, 'me']); 
 Route::apiResource('/books', BookController::class);
+Route::apiResource('/authors', AuthorController::class);
